816060dc74c2a86df8eb6f75b65e5ce577e72c7e
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-09-25 20:59:53 +0700

    Done Latihan 1

--- tutorial-3/src/main/java/com/apap/tutorial3/controller/CarController.java
+++ tutorial-3/src/main/java/com/apap/tutorial3/controller/CarController.java
@@ -2,10 +2,12 @@ package com.apap.tutorial3.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,11 +30,19 @@ public class CarController {
 		return "add";
 	}
 	
-	@RequestMapping("/car/view")
-	public String view(@RequestParam("id") String id, Model model) {
-		CarModel archive = mobilService.getCarDetail(id);
+	@RequestMapping(value = {"car/view/", "car/view/{id}"})
+	public String view(@PathVariable Optional<String> id, Model model) {
+		CarModel archive = null;
+		String isIdAda = null;
+		if (id.isPresent()) {
+			archive = mobilService.getCarDetail(id.get());
+			isIdAda = "ada ID";
+			model.addAttribute("id", id.get());
+		}
 		
 		model.addAttribute("car", archive);
+		model.addAttribute("isIdAda", isIdAda);
+		
 		return "view-car";
 	}
 	
